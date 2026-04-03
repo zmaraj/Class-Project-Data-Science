3d95e6d35beaf51b241ac38178162248d364fb11
zmaraj <zsmaraj99@gmail.com>
2026-04-03 16:05:38 -0400

    cleaned-data

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-# test
+import pandas as pd
+
+# Load both files
+
+red = pd.read_csv('winequality-red.csv', sep=';')
+
+white = pd.read_csv('winequality-white.csv', sep=';')
+
+# Add a type column to each
+
+red['type'] = 'red'
+
+white['type'] = 'white'
+
+# Combine them into one dataframe
+
+wine = pd.concat([red, white], ignore_index=True)
+
+# Save as cleaned_wine.csv
+
+wine.to_csv('cleaned_wine.csv', index=False)
+
+print(wine.shape)  # should show (6497, 13)
